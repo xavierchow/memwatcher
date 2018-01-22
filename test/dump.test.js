b1375667535a157e55cb4a06a55c54c142cf8ae5
Xavier Zhou <xiayezhou@gmail.com>
2018-01-22 14:43:23 +0800

    Refactor test code

---
+++ test/dump.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+const leaker = require('./fixture/leaker')
+const watcher = require('../index')
+const test = require('ava')
+const fs = require('fs')
+
+test.cb(t => {
+  const dumpFile = '/tmp/test.heapsnapshot'
+  process.env.MONITOR_MEM = 'on'
+  const opt = {
+    dumpFile,
+    callback: () => {
+      t.true(fs.existsSync(dumpFile))
+      t.end()
+    }
+  }
+  watcher(opt);
+  leaker.start();
+});
+
